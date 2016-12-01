7dbcea66b63aa0cfaf9e4bbcec049d8ef3b60295
Nick Matantsev <nick.m@myplanetdigital.com>
2016-11-30 19:48:13 -0500

    Rename FB input event webhook listener to reflect the streaming API nature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var WebHookRouter = require('./lib/WebHookRouter');
+var FBInputStream = require('./lib/FBInputStream');
 
 function fail(msg) {
     throw new Error(msg);
@@ -8,14 +8,14 @@ function fail(msg) {
 
 var appSecret = process.env.APP_SECRET || fail('missing app secret');
 
-var fbInputStream = new WebHookRouter('mytokenisgoodya', appSecret);
+var fbInputStream = new FBInputStream('mytokenisgoodya', appSecret);
 fbInputStream.on('data', function (data) {
     console.log(data);
 });
 
 var app = express();
 
-app.use('/fb-webhook', fbInputStream.router);
+app.use('/fb-webhook', fbInputStream.webhookRouter);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('started');
