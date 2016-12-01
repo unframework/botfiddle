288d69e77d03e5d183919fee64d195209563fb98
Nick Matantsev <nick.m@myplanetdigital.com>
2016-11-30 20:05:10 -0500

    Add simple send API

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 
 var FBInputStream = require('./lib/FBInputStream');
+var FBUserOutputStream = require('./lib/FBUserOutputStream');
 
 function fail(msg) {
     throw new Error(msg);
 }
 
 var appSecret = process.env.APP_SECRET || fail('missing app secret');
+var pageToken = process.env.PAGE_TOKEN || fail('missing page token');
 
 var fbInputStream = new FBInputStream('mytokenisgoodya', appSecret);
 fbInputStream.on('data', function (data) {
     console.log(data);
+
+    var senderId = data.sender.id;
+
+    var outputStream = new FBUserOutputStream(pageToken, senderId);
+    outputStream.write({ text: 'Hi there!' });
 });
 
 var app = express();
