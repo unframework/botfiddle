dce766937395ec799bcb537260bf369269258187
Nick Matantsev <nick.matantsev@gmail.com>
2016-12-04 18:50:32 -0500

    Componentize Messenger session area affordance

--- client.js
+++ client.js
@@ -3,32 +3,16 @@ var Writable = require('stream').Writable;
 var vdomLive = require('vdom-live');
 
 var Workspace = require('./lib/workspace/Workspace');
-var FBOptInWidget = require('./lib/FBOptInWidget');
+var MessengerSession = require('./lib/workspace/MessengerSession');
 var ACEEditorWidget = require('./lib/ACEEditorWidget');
 
 var Server = require('__server');
 
 var SCRIPT = 'input.on(\'data\', function (data) {\n    console.log(data);\n    output.write({ text: \'Hi from BotFiddle browser!\' });\n});\n';
 
-// FB SDK
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-var whenFBLoaded = new Promise(function (resolve) {
-    window.fbAsyncInit = function () {
-        resolve();
-    };
-});
-
 vdomLive(function (renderLive, h) {
     var server = new Server();
-    var optInStatus = false;
-    var optInWidget = null;
+    var messengerSession = new MessengerSession();
     var editorWidget = new ACEEditorWidget(SCRIPT);
     var eventLog = [];
 
@@ -72,20 +56,12 @@ vdomLive(function (renderLive, h) {
     };
 
     server.getInfo().then(function (info) {
-        whenFBLoaded.then(function () {
-            FB.init({
-                appId: info.fbAppId,
-                xfbml: false, // no parsing needed yet
-                version: "v2.6"
-            });
-
-            optInWidget = new FBOptInWidget(info.fbAppId, info.fbMessengerId, info.id);
-        });
+        messengerSession.initialize(info.fbAppId, info.fbMessengerId, info.id);
     });
 
     server.getEvents().then(function (emitter) {
         emitter.on('data', function (data) {
-            optInStatus = true;
+            messengerSession.logIncomingData(data);
 
             // skip initial marker packet
             if (Object.keys(data).length < 1) {
@@ -108,9 +84,7 @@ vdomLive(function (renderLive, h) {
             h,
             editorWidget,
             h('button', { onclick: function () { runScript(); } }, 'Go!'),
-            optInStatus ? 'Opted in!' : (
-                optInWidget ? [ 'Start new session: ', optInWidget ] : 'Loading...'
-            ),
+            messengerSession.render(h),
             h('ul', { style: { border: '1px solid #eee' } }, eventLog.map(function (entry) {
                 return h('li', entry);
             }))
