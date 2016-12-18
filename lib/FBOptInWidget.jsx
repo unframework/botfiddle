09a84ded0e6640efe2163307632d5ca96ad6ca35
Nick Matantsev <nick.matantsev@gmail.com>
2016-12-18 13:07:17 -0500

    Convert opt-in widget component to pure function

--- lib/FBOptInWidget.jsx
+++ lib/FBOptInWidget.jsx
@@ -1,29 +1,18 @@
 var React = require('react');
 
-// @todo convert to pure function
-class FBOptInWidget extends React.PureComponent {
-    constructor({ fbAppId, fbMessengerId, payload }) {
-        super();
+function FBOptInWidget({ fbAppId, fbMessengerId, payload }) {
+    return <div style={{
+        display: 'inline-block',
+        width: FBOptInWidget.widthPx + 'px',
+        height: FBOptInWidget.heightPx + 'px'
+    }} ref={(dom) => {
+        if (!dom) {
+            return;
+        }
 
-        this._fbAppId = fbAppId;
-        this._fbMessengerId = fbMessengerId;
-        this._payload = payload;
-    }
-
-    render() {
-        return <div style={{
-            display: 'inline-block',
-            width: FBOptInWidget.widthPx + 'px',
-            height: FBOptInWidget.heightPx + 'px'
-        }} ref={(dom) => {
-            if (!dom) {
-                return;
-            }
-
-            dom.innerHTML = '<div class="fb-send-to-messenger" messenger_app_id="' + this._fbAppId + '" page_id="' + this._fbMessengerId + '" data-ref="' + this._payload + '" color="blue" size="xlarge"></div>';
-            window.FB.XFBML.parse(dom);
-        }}></div>;
-    }
+        dom.innerHTML = '<div class="fb-send-to-messenger" messenger_app_id="' + fbAppId + '" page_id="' + fbMessengerId + '" data-ref="' + payload + '" color="blue" size="xlarge"></div>';
+        window.FB.XFBML.parse(dom);
+    }}></div>;
 }
 
 // well-known FB-defined size, exposed to help lay out containing elements
