8a5feec12cec18b191b22fdc29f9e84a7e79dd34
Nick Matantsev <nick.matantsev@gmail.com>
2016-12-23 22:37:54 -0500

    Fix hover state on script run button

--- lib/workspace/ScriptRunButton.jsx
+++ lib/workspace/ScriptRunButton.jsx
@@ -92,57 +92,9 @@ class ScriptRunButton extends React.Component {
         super();
 
         this.state = {
-            isHovering: false,
-            reactionTimeoutId: null
+            isHovering: false
         };
     }
-
-    onClick() {
-        // set up reaction timeout
-        var timeoutId = setTimeout(() => {
-            // ignore if stale timeout
-            if (this.state.reactionTimeoutId !== timeoutId) {
-                return;
-            }
-
-            this.setState({ reactionTimeoutId: null });
-        }, 300);
-
-        this.setState({ reactionTimeoutId: timeoutId });
-
-        this.props.onGoClick(this.props.editor.getValue(), this.props.outputStream);
-    }
-
-    // render() {
-    //     var isDisabled = this.state.reactionTimeoutId !== null || !this.props.editor || !this.props.outputStream;
-
-    //     return <Hover onStart={(hoverState) => {
-    //         this.setState({ isHovering: true });
-
-    //         hoverState.whenEnded.then(() => {
-    //             this.setState({ isHovering: false });
-    //         });
-    //     }}><button
-    //         disabled={isDisabled}
-    //         onClick={() => this.onClick()}
-    //         style={{
-    //             display: 'block',
-    //             margin: 0,
-    //             border: 0,
-    //             padding: '10px 20px',
-    //             lineHeight: '16px',
-    //             verticalAlign: 'middle',
-    //             width: '80px',
-    //             background: isDisabled ? '#c0e0f0' : (this.state.isHovering ? '#90d0e0' : '#80c0d0'),
-    //             cursor: isDisabled ? 'auto' : 'pointer',
-    //             borderRadius: '3px',
-    //             textAlign: 'center',
-    //             color: '#fff'
-    //         }}
-    //     >
-    //         { this.state.reactionTimeoutId !== null ? '😍' : 'Go!' }
-    //     </button></Hover>;
-    // }
 
     render() {
         const style = {
@@ -160,7 +112,13 @@ class ScriptRunButton extends React.Component {
             color: '#fff'
         };
 
-        return <SubmittableAction onSubmit={() => {
+        return <Hover onStart={(hoverState) => {
+            this.setState({ isHovering: true });
+
+            hoverState.whenEnded.then(() => {
+                this.setState({ isHovering: false });
+            });
+        }}><SubmittableAction onSubmit={() => {
             this.props.onGoClick(this.props.editor.getValue(), this.props.outputStream);
         }} prompt={(actionState) => {
             const isDisabled = !this.props.editor || !this.props.outputStream;
@@ -179,7 +137,7 @@ class ScriptRunButton extends React.Component {
             }, 300);
 
             return <button style={style}>😍</button>;
-        }} />;
+        }} /></Hover>;
     }
 }
 
